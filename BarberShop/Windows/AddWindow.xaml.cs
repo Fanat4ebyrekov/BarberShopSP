a45d7ff8045aac6d0f3709ce31bde7c5613acdea
Chebyrek <Chebyrek@DESKTOP-A1LC2FD>
2022-02-02 15:26:22 +0300

    Добавьте файлы проекта.

---
+++ BarberShop/Windows/AddWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static BarberShop.ClassEntities;
+using BarberShop.EF;
+
+namespace BarberShop.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        public AddWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (!string.IsNullOrWhiteSpace(FName.Text))
+            {
+                employee.FName = FName.Text;
+            }
+            else
+            {
+                MessageBox.Show("Вы не ввели имя");
+                return;
+            }
+            if (!string.IsNullOrWhiteSpace(LName.Text))
+            {
+                employee.LName = LName.Text;
+            }
+            else
+            {
+                MessageBox.Show("Вы не ввели фамилию");
+                return;
+            }
+            if (!string.IsNullOrWhiteSpace(TPhone.Text))
+            {
+                employee.TPhone = TPhone.Text;
+            }
+            else
+            {
+                MessageBox.Show("Вы не ввели номер");
+                return;
+            }
+
+            MessageBox.Show("Пользователь добавлен");
+            context.Client.Add(employee);
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            this.Hide();
+            PersonalWindow personalWindow = new PersonalWindow();
+            personalWindow.ShowDialog();
+            this.Close();
+
+
+        }
+    }
+}
